bb72f15389d509f4003825d7fda0e3d7f18676ea
neokdev <neokdev@gmail.com>
2018-01-29 14:52:04 +0100

    testing features Twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,9 +10,10 @@ namespace App\Controller;
 
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -27,8 +28,14 @@ class ArticleController
      */
     public function show($slug)
     {
-        return new Response(sprintf(
-            'Future page the article: %s',
-            $slug));
+        $comments = [
+            'We shall say \'Ni\' again to you, if you do not appease us. You can\'t expect to wield supreme power just \'cause some watery tart threw a sword at you! …Are you suggesting that coconuts migrate? Well, we did do the nose.',
+'Camelot! The Lady of the Lake, her arm clad in the purest shimmering samite, held aloft Excalibur from the bosom of the water, signifying by divine providence that I, Arthur, was to carry Excalibur. That is why I am your king.',
+'Who\'s that then? But you are dressed as one… Well, what do you want? Oh, ow! We want a shrubbery!! Oh! Come and see the violence inherent in the system! Help, help, I\'m being repressed!'
+        ];
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
+    ]);
     }
 }
