6e6fe840c5e0edf10057ea932e430a0df1c6321d
neokdev <neokdev@gmail.com>
2018-03-22 16:00:44 +0100

    added missing trans

--- src/Validator/Constraints/IsValidName.php
+++ src/Validator/Constraints/IsValidName.php
@@ -18,8 +18,7 @@ class IsValidName extends Constraint
      * Any public properties become valid options for the annotation.
      * Then, use these in your validator class.
      */
-//    TODO message
-    public $message = 'The value "{{ value }}" is not valid.';
+    public $message = 'nameNotValid';
 
     /**
      * @return mixed|string
